77028a7c9f78fe01662c0a176e69594339675113
Kevin Coleman <kevincolemaninc@gmail.com>
2014-01-22 12:54:35 -0500

    fix bootstrap install

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Mew::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'glucose_levels#index'
+  root 'high_voltage/pages#show', id: 'home'
 
   post 'glucose_levels' => 'glucose_levels#create'
 
