fe56ac29405042730e13ea195b68a4a2f48226b2
Marie <winter.sparklii@gmail.com>
2014-01-22 18:18:10 -0500

    add model validation

--- app/models/communication_device.rb
+++ app/models/communication_device.rb
@@ -1,4 +1,6 @@
 class CommunicationDevice < ActiveRecord::Base
+	validates :cdid, presence: true
+	
 	has_many :measurements
 	has_and_belongs_to_many :meters
 	has_and_belongs_to_many :patients
--- app/models/meter.rb
+++ app/models/meter.rb
@@ -1,4 +1,7 @@
 class Meter < ActiveRecord::Base
+	validates :mid, presence: true
+	validates :bluetooth_mac, presence: true
+
 	has_many :measurements
 	has_and_belongs_to_many :patients
 	has_and_belongs_to_many :communication_device
