32b5eb631afea5405b9b089de9bfc014c96555de
Kevin Coleman <kevincolemaninc@gmail.com>
2014-01-23 11:06:55 -0500

    add case manager to measurement

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -4,7 +4,8 @@ class Measurement < ActiveRecord::Base
 	validates :code_number, presence: true
 	validates :reading_type, presence: true
 	validates :communication_device_id, presence: true
-	validates :meter_id, presence: true
+	validates :meter_id, presence: true, unless: :case_manager_id
+	validates :case_manager_id, presence: true, unless: :meter_id
 	validates :patient_id, presence:true
 
 	belongs_to :communication_device
