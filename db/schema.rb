18af0d1857e30300a16659cbed64ed217ad01036
Kevin Coleman <kevincolemaninc@gmail.com>
2014-01-23 11:12:53 -0500

    patients can be archived

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140123160215) do
+ActiveRecord::Schema.define(version: 20140123161115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,6 +102,7 @@ ActiveRecord::Schema.define(version: 20140123160215) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "archived_at"
   end
 
   add_index "patients", ["email"], name: "index_patients_on_email", unique: true, using: :btree
