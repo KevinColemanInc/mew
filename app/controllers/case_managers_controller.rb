83709e3d6ffee0b21a118b6935536c3ed3f55aad
Kevin Coleman <kevincolemaninc@gmail.com>
2014-01-25 01:09:37 -0500

    case manager editing

--- app/controllers/case_managers_controller.rb
+++ app/controllers/case_managers_controller.rb
@@ -1,7 +1,9 @@
+require 'securerandom'
+
 class CaseManagersController < ApplicationController
   before_action :set_case_manager, only: [:show, :edit, :update, :destroy]
   before_filter :authenticate_user!
-  load_and_authorize_resource
+  
 
   # GET /case_managers
   def index
@@ -10,6 +12,7 @@ class CaseManagersController < ApplicationController
 
   # GET /case_managers/1
   def show
+
   end
 
   # GET /case_managers/new
@@ -19,15 +22,31 @@ class CaseManagersController < ApplicationController
 
   # GET /case_managers/1/edit
   def edit
+
   end
 
   # POST /case_managers
   def create
     @case_manager = CaseManager.new(case_manager_params)
+    @case_manager.password = SecureRandom.hex
+    if params[:case_manager][:archive] == "1"
+      @case_manager.archive
+    else
+      @case_manager.unarchive
+    end
+
+    if !(@case_manager.has_role? :case_manager_admin) && params[:case_manager][:is_an_admin] == "1"
+      @case_manager.roles << :case_manager_admin
+    elsif (@case_manager.has_role? :case_manager_admin) && params[:case_manager][:is_an_admin] == "0"
+      @case_manager.roles.delete(:case_manager_admin)
+    end
 
     if @case_manager.save
       redirect_to @case_manager, notice: 'Case manager was successfully created.'
     else
+      puts @case_manager.errors.inspect
+      puts
+      puts
       render action: 'new'
     end
   end
@@ -35,6 +54,22 @@ class CaseManagersController < ApplicationController
   # PATCH/PUT /case_managers/1
   def update
     if @case_manager.update(case_manager_params)
+      if params[:case_manager][:archive] == "1"
+        @case_manager.archive
+        @case_manager.save
+      else
+        @case_manager.unarchive
+        @case_manager.save
+      end
+
+      if !(@case_manager.has_role? :case_manager_admin) && params[:case_manager][:is_an_admin] == "1"
+        @case_manager.roles << :case_manager_admin
+      elsif (@case_manager.has_role? :case_manager_admin) && params[:case_manager][:is_an_admin] == "0"
+        @case_manager.roles.delete(:case_manager_admin)
+      end
+
+      @case_manager.save
+
       redirect_to @case_manager, notice: 'Case manager was successfully updated.'
     else
       render action: 'edit'
@@ -55,6 +90,6 @@ class CaseManagersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def case_manager_params
-      params.require(:case_manager).permit(:email, :first_name, :last_name)
+      params.require(:case_manager).permit(:email, :first_name, :last_name, :phone_number)
     end
 end
