80de2990adcf65e32066dd5f8731194da4111e35
Marie <winter.sparklii@gmail.com>
2014-01-26 11:13:37 -0500

    missed a file

--- app/assets/javascripts/app/controllers/member_groups_controller.js
+++
@@ -1,4 +0,0 @@
-app.controller('MemberGroupsCtrl', ['$scope', '$resource', function($scope, $resource) {
-  var MemberGroups = $resource('/member_groups.json');
-  $scope.member_groups = MemberGroups.query();
-}]);
