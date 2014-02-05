fa9678a5d698554493e4b44ac3f5b1cb32809abf
Kevin Coleman <c.programer@gmail.com>
2014-02-05 10:39:14 -0500

    full table search with one box, row count, and better CSS for table sorting

--- app/assets/javascripts/app/controllers/CaseManagersCtrl.js
+++ app/assets/javascripts/app/controllers/CaseManagersCtrl.js
@@ -1,28 +1,31 @@
 app.controller('CaseManagersCtrl', ['$scope', '$resource', '$filter', 'ngTableParams', function($scope, $resource, $filter, ngTableParams) {
   var CaseManager = $resource('/case_managers.json');
 
+  $scope.$watch("filter.$", function () {
+      $scope.tableParams.reload();
+    });
+
   $scope.case_managers = CaseManager.query(function(response)
     {
       $scope.tableParams = new ngTableParams({
           page: 1,            // show first page
           count: 10,          // count per page
           sorting: {
-            name: 'asc'     // initial sorting
+            full_name: 'asc'     // initial sorting
           }
       }, {
           total: $scope.case_managers.length, // length of data
           getData: function($defer, params) {
 
-            var filteredData = params.filter() ?
-                    $filter('filter')($scope.case_managers, params.filter()) :
-                    $scope.case_managers;
+            var filteredData = $filter('filter')($scope.case_managers, $scope.filter);
+
             var orderedData = params.sorting() ?
                                 $filter('orderBy')(filteredData, params.orderBy()) :
                                 filteredData;
             params.total(orderedData.length); // set total for recalc pagination
 
             $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
-          }
+          }, $scope: $scope
       });
     });
 
