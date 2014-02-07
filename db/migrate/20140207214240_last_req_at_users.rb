f2ee49041c262e237ce05d00638944078585cbe1
Kevin Coleman <kevin.coleman@gatech.edu>
2014-02-07 16:45:32 -0500

    add last requested at to users

---
+++ db/migrate/20140207214240_last_req_at_users.rb
@@ -0,0 +1,5 @@
+class LastReqAtUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :last_request_at, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140207022908) do
+ActiveRecord::Schema.define(version: 20140207214240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,6 +118,7 @@ ActiveRecord::Schema.define(version: 20140207022908) do
     t.datetime "date_of_birth"
     t.integer  "roles_mask"
     t.text     "note"
+    t.datetime "last_request_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
