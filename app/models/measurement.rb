9375e2aad4ecb7b371431f29f9e7c070e80c71f4
Kevin Coleman <kevin.coleman@gatech.edu>
2014-03-06 12:16:19 -0500

    fix measurement posting

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -4,7 +4,7 @@ class Measurement < ActiveRecord::Base
   validates :measured_at, presence: true
   validates :hash, presence: true
 	validates :code_number, presence: true
-	validates :reading_type, presence: true, inclusion: { in: %w(AC Normal PC QC), message: "%{value} is not a reading type" }
+	validates :reading_type, presence: true, inclusion: { in: %w(AC Normal PC QC), message: "%{value} is not AC, Normal, PC, or QC." }
 	validates :meter_id, presence: true, unless: :case_manager_id
   validates :case_manager_id, presence: true, unless: :meter_id
   validates :member_id, presence: true
@@ -26,6 +26,10 @@ class Measurement < ActiveRecord::Base
     member.update_attributes(preferred_meter_id: self.meter.id) if meter
   end
 
+  def display_name
+    "#{glucose_value} mg/dL at #{measured_at.strftime('%-m/%e/%y %l:%M %P')}"
+  end
+
   private
   def measurement_must_be_unique
     if Measurement.where(token: self.token).count > 0
@@ -39,9 +43,6 @@ class Measurement < ActiveRecord::Base
     end
   end
   
-  def display_name
-    "#{glucose_value} mg/dL at #{measured_at.strftime('%-m/%e/%y %l:%M %P')}"
-  end
-
+  
 end
 
